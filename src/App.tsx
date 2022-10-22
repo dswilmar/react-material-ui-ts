8e714617288317da9a147c6e619b0a6e3743883e
Wilmar dos Santos <dswilmar@gmail.com>
2022-10-22 09:59:56 -0300

    Feat: Adicionado tema light e o provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
+import { ThemeProvider } from "@mui/material";
 import { BrowserRouter } from "react-router-dom";
 import { AppRoutes } from "./routes";
+import { lightTheme } from "./shared/themes";
 
-export const App = () => {
+const App = () => {
   return (
-    <BrowserRouter>
-      <AppRoutes />
-    </BrowserRouter>
+    <ThemeProvider theme={lightTheme}>
+      <BrowserRouter>
+        <AppRoutes />
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
 
